5785f002e1540d02d0c508b211f15a5015fb8bb6
Sayali R <sayalirole5@gmail.com>
2026-03-05 11:53:05 +0000

    on home page remove trusted by experts. Used by the leaders. and What ou

--- src/components/ui/testimonials-with-marquee.tsx
+++ src/components/ui/testimonials-with-marquee.tsx
@@ -25,15 +25,6 @@ export function TestimonialsSection({
       className
     )}>
       <div className="mx-auto flex max-w-container flex-col items-center gap-4 text-center sm:gap-16">
-        <div className="flex flex-col items-center gap-4 px-4 sm:gap-8">
-          <h2 className="max-w-[720px] text-3xl font-semibold leading-tight sm:text-5xl sm:leading-tight text-secondary">
-            {title}
-          </h2>
-          <p className="text-md max-w-[600px] font-medium text-muted-foreground sm:text-xl">
-            {description}
-          </p>
-        </div>
-
         <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
           <div className="group flex overflow-hidden p-2 [--gap:1rem] [gap:var(--gap)] flex-row [--duration:240s]">
             <div className="flex shrink-0 justify-start [gap:var(--gap)] animate-marquee flex-row group-hover:[animation-play-state:paused]">
