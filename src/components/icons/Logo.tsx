1c97415ca5702c61af099001ce956c221732d0d2
Sayali R <sayalirole5@gmail.com>
2026-03-06 05:26:13 +0000

    use this as logo

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -13,7 +13,7 @@ export const Logo = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => 
         <text x="15" y="58" fontFamily="Inter, sans-serif" fontWeight="bold" fontSize="55" fill="white">G</text>
         
         <rect x="80" y="0" width="80" height="70" fill="white" stroke="#fe7823" strokeWidth="3"/>
-        <text x="95" y="58" fontFamily="Inter, sans-fonserif" fontWeight="bold" fontSize="55" fill="#1e4c87">G</text>
+        <text x="95" y="58" fontFamily="Inter, sans-serif" fontWeight="bold" fontSize="55" fill="#1e4c87">G</text>
       </g>
       
       <text x="40" y="110" fontFamily="Inter, sans-serif" fontWeight="bold" fontSize="40">
